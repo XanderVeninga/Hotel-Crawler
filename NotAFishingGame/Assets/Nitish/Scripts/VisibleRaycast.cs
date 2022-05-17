631b9abc6982c13b48814dee6d134b3999a0ee90
Nitish <69513631+Redbruh@users.noreply.github.com>
2022-05-17 12:41:18 +0100

    script is paar keer verwijdert en toegevoegd

--- NotAFishingGame/Assets/Nitish/Scripts/VisibleRaycast.cs
+++ NotAFishingGame/Assets/Nitish/Scripts/VisibleRaycast.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Visibleraycast : MonoBehaviour
+public class VisibleRaycast : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
